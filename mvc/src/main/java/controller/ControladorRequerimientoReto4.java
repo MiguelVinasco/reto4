4722f0dd50767c9e014631288106effad436e085
MiguelVinasco <miguelvinasco99@gmail.com>
2021-08-20 12:23:11 -0500

    Reto 4 con el segundo requerimiento

--- mvc/src/main/java/controller/ControladorRequerimientoReto4.java
+++ mvc/src/main/java/controller/ControladorRequerimientoReto4.java
@@ -3,6 +3,12 @@ package controller;
 // tiene que interactuar con los modelos ya asignados
 import model.dao.MaterialNacionalDao;
 import model.vo.MaterialNacional;
+
+//segundo modelo
+import model.dao.ProyectoRankeadoComprasDao;
+import model.vo.ProyectoRankeadoCompras;
+
+
 import java.util.ArrayList;
 import java.sql.SQLException;
 
@@ -12,9 +18,15 @@ public class ControladorRequerimientoReto4 {
 
     private final MaterialNacionalDao MaterialNacionalDao;
 
+    private final ProyectoRankeadoComprasDao ProyectoRankeadoComprasDao;
+
     public ControladorRequerimientoReto4(){
 
         this.MaterialNacionalDao = new MaterialNacionalDao();
+
+        this.ProyectoRankeadoComprasDao = new ProyectoRankeadoComprasDao();
+
+        
     }
 
     public ArrayList<MaterialNacional> consultarMaterialesNacionalesComprados() throws SQLException {
@@ -22,11 +34,9 @@ public class ControladorRequerimientoReto4 {
         return this.MaterialNacionalDao.topMaterialesNacionales();
     }
 
-    // public ArrayList<ProyectoRankeadoCompras> consultarProyectosComprasGranito() throws SQLException {
-    //     //.
-    //     //.
-    //     //.
-    // }
+    public ArrayList<ProyectoRankeadoCompras> consultarProyectosComprasGranito() throws SQLException {
+        return this.ProyectoRankeadoComprasDao.topProyectosComprasGranito();
+    }
 
     // public ArrayList<CargoAsignacion> consultarCargosMenosAsignados() throws SQLException {
     //     //.
